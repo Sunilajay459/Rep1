c7a4682c6798325537abe39dc673b58f42087f81
sunil <sunil.ajay1@gmail.com>
2025-05-26 12:07:38 +0530

    first commit

---
+++ fact3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int number;
+    int factorial = 1;    
+    printf("Enter a number to calculate its factorial: ");
+    scanf("%d", &number);
+    
+    if (number < 0) {
+        printf("Error: Cannot calculate factorial of negative numbers!\n");
+        return 1;
+    }    
+    printf("Calculating factorial of %d\n", number);
+    
+    for (int i = 1; i <= number; i++) {
+        factorial *= i;
+    }    
+    printf("Factorial of %d is %d\n", number, factorial);
+    
+    return 0;
+}
